f49cda4de13b45029f273be384ce3fdab948e3da
Riccardo <riccardocalligaro@gmail.com>
2017-12-19 06:47:52 +0100

    Initial commit

---
+++ stackarduino.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+
+Servo servo1;
+
+void setup()
+{
+  servo1.attach(9);
+    servo1.write(125);
+  delay(5000);
+  servo1.write(90);
+  delay(200);
+    servo1.write(125);
+  delay(700);
+
+}
+
+void loop()
+{
+  servo1.write(90);  //1
+  delay(200);
+    servo1.write(125);
+  delay(600);
+}
